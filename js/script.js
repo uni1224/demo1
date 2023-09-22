fa09829f543bb38d9df5ebe9b5f92a5841066a0c
uni1224 <saekahane99@gmail.com>
2023-09-22 11:47:43 +0900

    colorcode

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ const colorBg = () => {
     //カラーコードを表示
     if (color.value === '#ffffff') {
         text.textContent = `カラーコード : ${color.value} (white)`;
-    } else if (color.value === '#000000') {
+    } else if (color.value === '#00000                         0') {
         text.textContent = `カラーコード : ${color.value} (black)`;
     } else {
         text.textContent = `カラーコード : ${color.value}`;
